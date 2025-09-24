a4ef439752220a8e8a0bb54486e39d4916b84c32
Fernando Ichazo <fernando.ichazo@ucb.edu.bo>
2025-09-24 12:54:09 -0400

    feat(api): Creacion de la clase dto para crear un nuevo servicio

---
+++ src/domain/dtos/servicio/crearServicioDto.ts
@@ -0,0 +1,21 @@
+export class CrearServicioDto{
+    private constructor(
+        public servicio: string,
+        public usuario: string,
+        public contrasenia: string
+    ){}
+
+    static create( object: {[key: string]: any}): [string?, CrearServicioDto?]{
+
+        const { servicio, usuario, contrasenia } = object
+
+        if( !servicio ) return ['El nombre del servicio es necesario', undefined];
+
+
+        return [undefined, new CrearServicioDto(
+            servicio,
+            usuario,
+            contrasenia
+        )];
+    }
+}
