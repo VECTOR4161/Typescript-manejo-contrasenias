0f3779f98764fcc32c82e31240e6455f4a5eccc1
Fernando Ichazo <fernando.ichazo@ucb.edu.bo>
2025-09-24 12:37:27 -0400

    feat(api): Creacion de archivos de barril en la capa data y domain, creacion de la entidad servicio en dominio

---
+++ src/domain/entities/servicio.ts
@@ -0,0 +1,8 @@
+export class Servicio{
+    constructor(
+        public id: number,
+        public servicio: string,
+        public usuario: string,
+        public contrasenia: string
+    ){}
+}
