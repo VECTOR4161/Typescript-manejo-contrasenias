e46ef0562e240587bae170b09708ae28ec8e0eac
Fernando Ichazo <fernando.ichazo@ucb.edu.bo>
2025-09-24 13:12:42 -0400

    feat(api): Adicion del repositorio de servicio

---
+++ src/domain/repositories/servicio.repository.ts
@@ -0,0 +1,10 @@
+import { ActualizarServicioDto, CrearServicioDto, Servicio } from "..";
+import { PaginacionDto } from "../../common";
+
+abstract class ServicioRepository{
+    abstract crearServicio(crearServicioDto: CrearServicioDto): Promise<Servicio>
+    abstract actualizarServicio(actualizarServicioDto: ActualizarServicioDto): Promise<Servicio>
+    abstract eliminarServicio(idServicio: number): Promise<boolean>
+    abstract obtenerServicio(idServicio: number): Promise<Servicio>
+    abstract obtenerServicios(paginacionDto: PaginacionDto): Promise<Servicio[]>
+}
